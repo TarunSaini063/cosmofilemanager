f0e65211ac0cb716ce698787469a8907eb8658cc
TarunSaini063 <tsaini063@outlook.com>
2021-03-27 23:45:45 +0530

    add commont font type and common node dimension

---
+++ src/win95/constants/Dimensions.java
@@ -0,0 +1,9 @@
+package win95.constants;
+
+import win95.controller.Controller;
+
+public class Dimensions {
+    public static final int LISTVIEW_ROWIMAGEVIEW = 25;
+    public static final int LISTVIEW_ROWCOUNT = 30;
+    public static final int PREVIEW_FILE_ICON = 70;
+}
---
+++ src/win95/constants/Fonts.java
@@ -0,0 +1,8 @@
+package win95.constants;
+
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class Fonts {
+    public static Font PREVIEW_FONT = Font.font("Comic Sans MS", FontWeight.BOLD, 12);
+}
