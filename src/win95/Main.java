f43884c8e228a34a55f44056f42c859c8ad25ab9
TarunSaini063 <tsaini063@outlook.com>
2021-03-27 08:43:39 +0530

    Fix main file path

--- src/win95/Main.java
+++ src/win95/Main.java
@@ -1,4 +1,4 @@
-package win95.model;
+package win95;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("./view/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.setMaximized(true);
