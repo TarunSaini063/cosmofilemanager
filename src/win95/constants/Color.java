0d77bc72038d71214ab81fa5db438b3d03218f2e
TarunSaini063 <tsaini063@outlook.com>
2021-03-28 23:56:19 +0530

    Add global color file

---
+++ src/win95/constants/Color.java
@@ -0,0 +1,7 @@
+package win95.constants;
+
+public class Color {
+    public static String LEFT_PANE_COLOR = "#D4DFE7";
+    public static String PREVIEW_PANE_COLOR = "#D4DFE7";
+    public static String MIDDLE_PANE_COLOR = " #EEDFEA";
+}
